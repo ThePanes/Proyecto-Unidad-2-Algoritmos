48875fcde6840bbdd2a7a7f7dc7cbd4503dd1e10
Oscar Montecinos <oscar.montecinos.guerrero@gmail.com>
2023-11-14 22:51:46 -0300

    Inicio Proyecto
    Inicio del Archivo proyecto, para esta primera instancia, inicie el proyecto en base al laboratorio 9 solo para una base de ciertas cosas que podrían ser útiles.

---
+++ ProyectoUnidad2.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+
+using namespace std;
+
+    struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insertar(Node* root,  int data){
+
+    if(root == NULL){
+        return createNode(data);
+    }
+
+    if(data < root->data){
+        root->left = insertar(root->left, data);
+    }else{
+        root->right = insertar(root->right, data);
+    }
+
+    return root;
+}
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+
+    inorder(root->right);
+}
+
+Node *DFS(Node* root, int value){
+
+    if(root == NULL || root->data == value){
+        return root;
+    }
+
+    Node* left_result = DFS(root->left, value);
+    if(left_result != NULL){
+        return left_result;
+    }
+    Node* right_result = DFS(root->right,value);
+    if(right_result != NULL){
+        return right_result;
+    }
+    return NULL;
+}
+
+Node* BFS(Node* root, int value){
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* current = q.front();
+        q.pop();
+        if(current->data == value){
+            return current;
+        }
+        if(current->left != NULL){
+            q.push(current->left);
+        }
+        if(current->right != NULL){
+            q.push(current->right);
+        }
+    }
+    return NULL;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        Node* temp = root->right;
+        while (temp->left != NULL) {
+            temp = temp->left;
+        }
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+int main() {
+
+    int numeros[] = {21,18,6,9,10,7,19,15,12,5};
+    int n  = sizeof(numeros)/sizeof(numeros[0]);
+
+    Node* root = NULL;
+
+    for(int i = 0; i < n; i++){
+        root = insertar(root,numeros[i]);
+    }
+
+    cout << "In Order" << endl;
+    inorder(root);
+    cout <<endl;
+
+    int numerobuscadoDFS = 18;
+
+    Node* resultanteDFS = DFS(root,numerobuscadoDFS);
+
+    cout <<"Busqueda por profundidad."<<endl;
+
+    if(resultanteDFS != NULL){
+        cout << "Numero encontrado:" << resultanteDFS->data <<endl;
+    }else{
+        cout << "No encontrado" <<endl;
+    }
+
+    int numerobuscadoBFS = 7;
+    Node* resultanteBFS = BFS(root,numerobuscadoBFS);
+    cout << "Busqueda por Amplitud."<<endl;
+    if(resultanteBFS != NULL){
+        cout << "Numero encontrado:" << resultanteBFS->data <<endl;
+    }else{
+        cout << "No encontrado" <<endl;
+    }
+
+    return 0;
+}
